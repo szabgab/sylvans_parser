c072c96486e4b7c49e6ac98addd81e7c2f352ad9
Sylvan Martin <slm338@cornell.edu>
2024-03-02 17:46:07 -0500

    Make parser more general

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@ pub enum ParsingError {
     NoSuchFlag(String)
 }
 
-pub fn parse(args: &Vec<String>, flags: Vec<String>) -> Result<Vec<String>, ParsingError> {
-    let mut params: Vec<String> = Vec::new();
+pub fn parse(args: &Vec<String>, flags: Vec<String>) -> Vec<Result<String, ParsingError>> {
+    let mut params: Vec<Result<String, ParsingError>> = Vec::new();
 
     // Yes, this is an O(n^2) time algorithm. Yes, it can be way faster. That's okay for now.
 
@@ -13,7 +13,7 @@ pub fn parse(args: &Vec<String>, flags: Vec<String>) -> Result<Vec<String>, Pars
         let mut found = false;
         for i in 0..(args.len() - 1) {
             if args[i] == flag {
-                params.push(args[i + 1].clone());
+                params.push(Ok(args[i + 1].clone()));
                 found = true;
                 break;
             }
@@ -24,10 +24,10 @@ pub fn parse(args: &Vec<String>, flags: Vec<String>) -> Result<Vec<String>, Pars
         }
 
         // if we get through the for loop, we couldn't find the argument!
-        return Err(ParsingError::NoSuchFlag(flag));
+        params.push(Err(ParsingError::NoSuchFlag(flag)));
     }
 
-    Ok(params)
+    params
 }
 
 #[cfg(test)]
@@ -39,11 +39,9 @@ mod tests {
     fn it_works() {
 
         let args = vec!["this".to_string(), "is".to_string(), "-a".to_string(), "test".to_string(), "and".to_string(), "thats".to_string(), "-i".to_string(), "the_path".to_string()];
-        let flags = vec!["-i".to_string(), "-a".to_string()];
-        let wrong_flags = vec!["-q".to_string()];
-        let expected = vec!["the_path".to_string(), "test".to_string()];
+        let flags = vec!["-i".to_string(), "-a".to_string(), "-q".to_string()];
+        let expected = vec![Ok("the_path".to_string()), Ok("test".to_string()), Err(ParsingError::NoSuchFlag("-q".to_string()))];
 
-        assert_eq!(parse(&args, flags), Ok(expected));
-        assert_eq!(parse(&args, wrong_flags), Err(ParsingError::NoSuchFlag("-q".to_string())));
+        assert_eq!(parse(&args, flags), expected);
     }
 }
